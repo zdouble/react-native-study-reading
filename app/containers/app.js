4eb1877be2692de7897172b7d129164c3f71163a
zdouble <872715485@qq.com>
2017-05-30 21:44:31 +0800

    redux test

--- app/containers/app.js
+++ app/containers/app.js
@@ -1,27 +1,13 @@
-//import liraries
-import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StackNavigator } from 'react-navigation'
+import Main from '../containers/main-containers.js'
 
-// create a component
-class App extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>App</Text>
-            </View>
-        );
+const App = StackNavigator({
+    Main: {
+        screen: Main,
+        navigationOptions: {
+            header: null
+        }
     }
-}
+})
 
-// define your styles
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
-    },
-});
-
-//make this component available to the app
-export default App;
+export default App
---
+++ app/containers/main-containers.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import Main from '../views/main'
+
+class MainContainers extends Component {
+    render() {
+        return (
+            <Main {...this.props}/>
+        );
+    }
+}
+export default connect(
+    ({user}) => ({user}),
+)(MainContainers)
