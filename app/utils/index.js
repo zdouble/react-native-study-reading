1e2cad2f87ee9cb9624c0aa31f8777be7ca8a1d6
zdouble <872715485@qq.com>
2017-06-29 18:14:27 +0800

    add utils dateFormat

---
+++ app/utils/index.js
@@ -0,0 +1,21 @@
+const dateFormat = (date) => {
+    let time = (Date.now() - new Date(date)) / 1000
+    if (time / 60 < 1) {
+        time = `${time}秒前`
+    } else if (time / (60 * 60) < 1) {
+        time = `${parseInt(time / 60)}分前`
+    } else if (time / (60 * 60 * 24) < 1) {
+        time = `${parseInt(time / (60 * 60))}小时前`
+    } else if (time / (60 * 60 * 24 * 30) < 1) {
+        time = `${parseInt(time / (60 * 60 * 24))}天前`
+    } else if (time / (60 * 60 * 24 * 30 * 365) < 1) {
+        time = `${parseInt(time / (60 * 60 * 24 * 30))}月前`
+    } else {
+        time = `${parseInt(time / (60 * 60 * 24 * 30 * 365))}年前`
+    }
+    return time
+}
+
+export {
+    dateFormat
+}
