1abfeda66cbf29dd11577ae9c7b939e31f87823b
zdouble <872715485@qq.com>
2017-06-29 18:15:42 +0800

    articeList

--- app/views/main/article-list.js
+++ app/views/main/article-list.js
@@ -1,26 +1,145 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    FlatList,
+    Image
 } from 'react-native'
 
+import { getArticleList } from '../../api'
+import { dateFormat } from '../../utils'
+
 class ArticleList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [],
+            refreshing: false,
+            moreLoading: false,
+            page: 1
+        }
+    }
+
+    componentWillMount() {
+        this.fetchDate()
+    }
+
+    fetchDate() {
+        getArticleList({ typeId: this.props.typeId, page: this.state.page })
+            .then(res => {
+                console.log(res)
+                let data = res.showapi_res_body.pagebean.contentlist.filter(item => !item.expire)
+                if (this.state.moreLoading) {
+                    this.setState({ data: [...this.state.data, ...data] })
+                } else {
+                    this.setState({ data })
+                }
+                this.setState({ moreLoading: false, refreshing: false })
+            })
+    }
+
+    _renderItem = ({ item: artilce }) => {
+        // console.log(artilce)
+        return (
+            <View style={styles.artilceContainer}>
+                <Image
+                    style={styles.artilceImage}
+                    source={{ uri: artilce.contentImg }}
+                />
+                <View style={styles.artilceContent}>
+                    <Text
+                        key={artilce._id}
+                        numberOfLines={2}
+                    >
+                        {artilce.title}
+                    </Text>
+                    <View style={styles.artilceInfo}>
+                        <Text style={styles.artilceSource}>{artilce.userName}</Text>
+                        <Text style={styles.artilceDate}>{dateFormat(artilce.date)}</Text>
+                    </View>
+                </View>
+
+            </View>
+        )
+    }
+
+    _onRefresh = () => {
+        this.setState({ refreshing: true, page: 1 }, () => {
+            this.fetchDate()
+        })
+    }
+    _onEndReached = () => {
+        this.setState({ moreLoading: true, page: ++this.state.page }, () => {
+            this.fetchDate()
+        })
+    }
+
+    _keyExtractor = (item, index) => item.id
+
+    renderContent() {
+        const data = this.state.data
+        if (!data) {
+            return null
+        }
+        return (
+            <FlatList
+                data={data}
+                renderItem={this._renderItem}
+                getItemLayout={(data, index) => ({ length: 87, offset: 87 * index, index })}
+                onRefresh={this._onRefresh}
+                onEndReached={this._onEndReached}
+                refreshing={this.state.refreshing}
+                keyExtractor={this._keyExtractor}
+            />
+        )
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>ArticleList1</Text>
+                {this.renderContent()}
             </View>
         )
     }
 }
 
+ArticleList.PropTypes = {
+    typeId: PropTypes.number
+}
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#fff'
+    },
+    artilceContainer: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#eee',
+        borderStyle: 'solid',
+        flexDirection: 'row',
+        padding: 10,
+        flex: 1
+    },
+    artilceImage: {
+        width: 88,
+        height: 66
+    },
+    artilceContent: {
+        flex: 1,
+        paddingLeft: 10,
+        justifyContent: 'space-between'
+    },
+    artilceInfo: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    artilceSource: {
+        color: '#87CEFA',
+        fontSize: 12
+    },
+    artilceDate: {
+        fontSize: 12
     }
 })
 
--- app/views/main/index.js
+++ app/views/main/index.js
@@ -1,18 +1,8 @@
 import React, { Component } from 'react'
-import {
-    View,
-    Text,
-    StyleSheet
-} from 'react-native'
 import ScrollableTabView, { ScrollableTabBar } from 'react-native-scrollable-tab-view'
-import { getArticleList } from '../../api'
 import ArticleList from './article-list.js'
 
 class Main extends Component {
-    componentWillMount() {
-        getArticleList({ typeId: 19, page: 20 })
-            .then(res => console.log(res))
-    }
     render() {
         return (
             <ScrollableTabView
@@ -22,10 +12,10 @@ class Main extends Component {
                 tabBarBackgroundColor="#fff"
                 style={{ padding: 0 }}
             >
-                <ArticleList tabLabel="test" />
-                <ArticleList tabLabel="test2" />
-                <ArticleList tabLabel="test3" />
-
+                <ArticleList
+                    tabLabel="test"
+                    typeId={14}
+                />
             </ScrollableTabView>
         )
     }
