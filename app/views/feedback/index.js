7ccd144be841fe9f63ccde7e6dc052161a61eade
zdouble <872715485@qq.com>
2017-07-01 18:02:31 +0800

    feedback

--- app/views/feedback/index.js
+++ app/views/feedback/index.js
@@ -1,18 +1,55 @@
 import React, { Component } from 'react'
 import Icon from 'react-native-vector-icons/Ionicons'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, TextInput, StyleSheet, ToastAndroid, Keyboard } from 'react-native'
+
+let feedbackText = ''
 
 class Feedback extends Component {
-    static navigationOptions = {
+    static navigationOptions = ({ navigation }) => ({
         title: '建议',
         tabBarIcon: ({ tintColor }) => (
             <Icon name="md-thumbs-up" size={25} color={tintColor} />
+        ),
+        headerRight: (
+            <Icon.Button
+                name="md-checkmark"
+                backgroundColor="transparent"
+                underlayColor="transparent"
+                activeOpacity={0.8}
+                onPress={() => {
+                    navigation.state.params.feedbackSubmit()
+                }}
+            />
         )
+    })
+
+    componentDidMount() {
+        this.props.navigation.setParams({ feedbackSubmit: this.feedbackSubmit })
     }
+
+    feedbackSubmit = () => {
+        if (feedbackText.trim() === '') {
+            ToastAndroid.show('请填写建议内容', ToastAndroid.SHORT)
+        } else {
+            ToastAndroid.show('您的问题已反馈，我们会及时跟进处理', ToastAndroid.SHORT)
+            this.input.clear()
+            Keyboard.dismiss()
+        }
+    }
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>Feedback</Text>
+                <TextInput
+                    ref={(ref) => { this.input = ref }}
+                    style={styles.input}
+                    maxLength={200}
+                    placeholder="请写下您宝贵的意见或建议，与iReading一起进步！"
+                    underlineColorAndroid="transparent"
+                    onChangeText={(text) => { feedbackText = text }}
+                    multiline
+                    autoFocus
+                />
             </View>
         )
     }
@@ -21,9 +58,11 @@ class Feedback extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50'
+        backgroundColor: '#fff'
+    },
+    input: {
+        flex: 1,
+        textAlignVertical: 'top'
     }
 })
 
